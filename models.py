42582664170224eed138bd26d6ba7693670e503e
idyweb <inyangidongesit22@gmail.com>
2025-10-28 18:53:57 +0100

    minor fixes

--- models.py
+++ models.py
@@ -1,9 +1,14 @@
 from typing import Annotated
 from datetime import datetime, timezone
 
-from fastapi import  Depends, FastAPI, HTTPException, Query
-from sqlmodel import Field, SQLModel, create_engine, Session, select
+from fastapi import  Depends
+from sqlmodel import Field, SQLModel, create_engine, Session
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///countries.db") 
 
 class Countries(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
@@ -18,11 +23,8 @@ class Countries(SQLModel, table=True):
     last_refreshed_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
 
 
-sqlite_file_name = "countries.db"
-sqlite_url = f"sqlite:///{sqlite_file_name }"
-
 connect_args = {"check_same_thread": False}
-engine = create_engine(sqlite_url, connect_args=connect_args)
+engine = create_engine(DATABASE_URL, connect_args=connect_args, echo=False)
 
 
 def create_db_and_tables():
